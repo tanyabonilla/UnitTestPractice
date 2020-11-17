0fe5093bda2d47d9abbe2d404bd94c74bfec1c33
tanyabonilla <tanyabellen@gmail.com>
2020-11-16 22:54:28 -0800

    changed sortDescending from bool return to void

--- Practice.cpp
+++ Practice.cpp
@@ -5,8 +5,7 @@ using std::string;
 
 // Receive three integers and rearrange their values so that they are in
 // descending order from greatest (first) to least (third)
-bool Practice::sortDescending(int & first, int & second, int & third)
-{
+void Practice::sortDescending(int & first, int & second, int & third) {
   if( first < second )
   {
     int temp = first;
@@ -25,16 +24,6 @@ bool Practice::sortDescending(int & first, int & second, int & third)
     second = third;
     third = temp;
   }
-
-  if((first > second) && (second > third))
-  {
-    return true;
-  }
-  else
-  {
-    return false;
-  }
-
 }
 
 // Receive a string and return whether or not it is strictly a palindrome,
